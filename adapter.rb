ae5f400786819ae618299684e6d461b52993f245
Charles Pernet <charlespernet@gmail.com>
2020-01-17 17:52:11 +0100

    WIP data from Iracing

---
+++ adapter.rb
@@ -0,0 +1,36 @@
+require 'json'
+
+class Adapter
+  def initialize(json)
+    @json = json
+  end
+
+  def call
+    laps = JSON.parse(json)
+    grouped_by_track = laps.group_by { |lap| lap["trackid"] }
+    best_laps(grouped_by_track)
+  end
+
+  private 
+
+  def best_lap_on_track(laps_data)
+    {
+      track_name: track_name(laps_data),
+      bestlap: best_lap(laps_data)
+    }
+  end
+
+  def best_laps
+    grouped_by_track.map { |_trackid, laps_data| best_lap_on_track(laps_data) }
+  end
+
+  def track_name(laps_data)
+    laps_data.first["trackname"].gsub("+", ' ')
+  end
+
+  def best_lap(laps_data)
+    laps_data.map { |lap_data| lap_data["bestlaptimeformatted"].gsub("%3A", '.') }.min
+  end
+
+  attr_reader :json
+end
---
+++ application.rb
@@ -0,0 +1,23 @@
+require_relative 'i_racing_data_for_user'
+
+class Application
+  def self.run
+    # parse for every users
+    # return Array of formatted Hash
+    
+
+    data = default_users.map { |user| IRacingDataForUser.new(user).call }
+    # Push Data to discord Bot
+  end
+
+  private
+
+  def self.default_users
+    [
+      OpenStruct.new({name: "Anthony", id: "406148"}), 
+      OpenStruct.new({name: "Charles", id: "404308"})
+    ]
+  end
+end
+
+p Application.run
